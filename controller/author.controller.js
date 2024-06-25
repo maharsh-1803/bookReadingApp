71585eb4223e6db0594d31bb6641b8cce572a96d
maharsh-1803 <maharshpatel1803@gmail.com>
2024-06-25 17:08:26 +0530

    changes done

--- controller/author.controller.js
+++ controller/author.controller.js
@@ -30,15 +30,28 @@ const registerAuthor = async(req,res)=>{
     }
 }
 
-const profileDisplay = async(req,res)=>{
-    const {id} = req.params;
+const profileDisplay = async (req, res) => {
+    const { id } = req.params;
+    const baseURL = 'https://bookingreadingapp.onrender.com/uploads'; 
+
     try {
         const author = await Author.findById(id);
-        return res.status(200).send(author)
+
+        if (!author) {
+            return res.status(404).send({ error: 'Author not found' });
+        }
+
+        let authorWithPhotoURL = {
+            ...author._doc,
+            photoURL: author.photo ? `${baseURL}/${author.photo}` : null
+        };
+
+        return res.status(200).send(authorWithPhotoURL);
     } catch (error) {
-        res.status(400).send({error:error.message})
+        return res.status(400).send({ error: error.message });
     }
-}
+};
+
 
 const loginAuthor = async(req,res)=>{
     const {mobile,name} = req.body;
@@ -76,10 +89,59 @@ const allAuthor = async(req,res)=>{
 }
 
 
+// const editAuthor = async (req, res) => {
+//     const { id } = req.params;
+//     const updateFields = req.body; 
+//     console.log(req.body);
+//     const file = req.file; 
+
+//     try {
+//         let author = await Author.findById(id);
+//         if (!author) {
+//             return res.status(404).send({ message: "Author not found with this ID" });
+//         }
+//         if (updateFields.name) {
+//             author.name = updateFields.name;
+//         }
+//         if (updateFields.dob) {
+//             author.dob = updateFields.dob;
+//         }
+//         if (updateFields.city) {
+//             author.city = updateFields.city;
+//         }
+//         if (updateFields.state) {
+//             author.state = updateFields.state;
+//         }
+//         if (updateFields.country) {
+//             author.country = updateFields.country;
+//         }
+//         if (updateFields.gender) {
+//             author.gender = updateFields.gender;
+//         }
+//         if (updateFields.status) {
+//             author.status = updateFields.status;
+//         }
+//         if (updateFields.mobile) {
+//             author.mobile = updateFields.mobile;
+//         }
+//         if (updateFields.email) {
+//             author.email = updateFields.email;
+//         }
+
+//         if (file) {
+//             author.photo = file.filename;
+//         }
+        
+//         await author.save();
+
+//         res.status(200).json({ message: "Author updated successfully", author });
+//     } catch (error) {
+//         res.status(400).send({ error: error.message });
+//     }
+// }
 const editAuthor = async (req, res) => {
     const { id } = req.params;
-    const updateFields = req.body; 
-    console.log(req.body);
+    const updateFields = { ...req.body }; 
     const file = req.file; 
 
     try {
@@ -87,39 +149,12 @@ const editAuthor = async (req, res) => {
         if (!author) {
             return res.status(404).send({ message: "Author not found with this ID" });
         }
-        if (updateFields.name) {
-            author.name = updateFields.name;
-        }
-        if (updateFields.dob) {
-            author.dob = updateFields.dob;
-        }
-        if (updateFields.city) {
-            author.city = updateFields.city;
-        }
-        if (updateFields.state) {
-            author.state = updateFields.state;
-        }
-        if (updateFields.country) {
-            author.country = updateFields.country;
-        }
-        if (updateFields.gender) {
-            author.gender = updateFields.gender;
-        }
-        if (updateFields.status) {
-            author.status = updateFields.status;
-        }
-        if (updateFields.mobile) {
-            author.mobile = updateFields.mobile;
-        }
-        if (updateFields.email) {
-            author.email = updateFields.email;
+        if (file) {
+            updateFields.photo = file.filename;
         }
 
-        if (file) {
-            author.photo = file.filename;
-        }
-        
-        await author.save();
+        // Find and update the author
+        author = await Author.findByIdAndUpdate(id, updateFields, { new: true });
 
         res.status(200).json({ message: "Author updated successfully", author });
     } catch (error) {
--- index.js
+++ index.js
@@ -14,6 +14,8 @@ require('dotenv').config()
 
 const app = express();
 
+app.use("/uploads", express.static("./uploads"));
+
 
 app.use(cors());
 app.use(express.json());
